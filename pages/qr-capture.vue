83d0d1cdb9cf4dfb2519d599137b71db397976e9
zawkhantmaung <zawkhaintmaung.cupkku@gmail.com>
2020-08-22 22:27:29 +0630

    [add] qr capture and dropzone

---
+++ pages/qr-capture.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+    <qrcode-capture @decode="onDecode" />
+  </div>
+</template>
+
+<script>
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
+export default {
+
+  components: {
+    QrcodeCapture,
+  },
+
+  data () {
+    return {
+      result: ''
+    }
+  },
+
+  methods: {
+    onDecode (result) {
+      this.result = result
+    }
+  }
+}
+</script>
